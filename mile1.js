860a93f55389c62afb30ec908e8a0d91652636fc
swethabattula8 <109943484+swethabattula8@users.noreply.github.com>
2022-08-21 22:08:08 +0530

    Add files via upload

---
+++ mile1.js
@@ -0,0 +1,11 @@
+<html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello />);
+</html>
